2dbfb2092353b377a8296e34ff751948a830b73f
Junaid S <intelpik@gmail.com>
2021-01-09 14:53:06 +0530

    Started creating Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Started</h1>
+    <div>
+     <Navbar/>
     </div>
   );
 }
