ad47b9acf109a6c17043125ce4316d28c9d5a2ac
Junaid S <intelpik@gmail.com>
2021-01-09 17:15:42 +0530

    added 3 horizontal  bar icon for hamburger menu and added basic style to navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
 import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-     <Navbar/>
+      <Navbar />
     </Router>
   );
 }
