3f979503ae492e2ee08f5eb4c27a6cf863063668
Junaid S <intelpik@gmail.com>
2021-01-10 20:49:31 +0530

    Project Finished

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,12 +1,12 @@
-import {useEffect} from "rect";
+import {useEffect} from "react";
 import {useLocation} from "react-router-dom";
 
-export default function scrollToTop(){
-    const {pathname} =useLocation()
+export default function ScrollToTop(){
+    const {pathname} =useLocation();
 
     useEffect(()=>{
-        window.scrollTo(0,0)
-    },[pathname])
+        window.ScrollTo(0,0)
+    },[pathname]);
 
 
     return null;
