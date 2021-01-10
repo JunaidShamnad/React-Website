eda3f1e5313203e3681719bb421ba13d666a7571
Junaid S <intelpik@gmail.com>
2021-01-10 13:29:59 +0530

    created basic structure and styling for infosection

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,27 +1,46 @@
-import React from 'react';
-import { Button } from 'react-scroll';
+import React from "react";
+import { Button } from "react-scroll";
+import {
+  InfoContainer,
+  InfoWrapper,
+  InfoRow,
+  Column1,
+  Column2,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  BtnWrap,
+  Img,
+  ImgWrap,
+} from "./InfoElements";
 
 const InfoSection = () => {
-    return (
-        <>
-            <InfoContainer id={id}>
-                <InfoWrapper>
-                    <InfoRow>
-                        <Column1>
-                            <TextWrapper>
-                                <TopLine>TopLine</TopLine>
-                                <Heading>Heading</Heading>
-                                <Subtitle>Subtitle</Subtitle>
-                                <BtnWrap>
-                                    <Button to="home">Button</Button>
-                                </BtnWrap>
-                            </TextWrapper>
-                        </Column1>
-                    </InfoRow>
-                </InfoWrapper>
-            </InfoContainer>
-        </>
-    )
-}
+  return (
+    <>
+      <InfoContainer>
+        <InfoWrapper>
+          <InfoRow>
+            <Column1>
+              <TextWrapper>
+                <TopLine>TopLine</TopLine>
+                <Heading>Heading</Heading>
+                <Subtitle>Subtitle</Subtitle>
+                <BtnWrap>
+                  <Button to="home"/>
+                </BtnWrap>
+              </TextWrapper>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <Img />
+              </ImgWrap>
+            </Column2>
+          </InfoRow>
+        </InfoWrapper>
+      </InfoContainer>
+    </>
+  );
+};
 
 export default InfoSection;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import HeroSection from "../components/HeroSection";
+import InfoSection from "../components/InfoSection";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 
@@ -14,6 +15,7 @@ const Home = () => {
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
       <HeroSection />
+      <InfoSection/>
     </>
   );
 };
