207d125b1065dd51872ecb2c12d24b9fc8af8cd3
Junaid S <intelpik@gmail.com>
2021-01-10 11:27:52 +0530

    added styles in heroElements.js and made the background darker

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,44 @@
+import React,{useState} from "react";
+import Video from "../../videos/video.mp4";
+import { Button } from "../ButtonElement";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
+
+const HeroSection = () => {
+
+const [hover,setHover]=useState(false)
+
+const onHover=()=>{
+    setHover(!hover)
+}
+
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Always Choose Good</HeroH1>
+        <HeroP>
+          We find solutions to everything for you.Build your dreams with us
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button to="signup" onMouseEnter={onHover}  onMouseLeave={onHover} primary='true' dark='true'>
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}{" "}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
